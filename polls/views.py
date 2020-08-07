d2c9489db594c86977b02e178ffb2fa9070057d0
Nicky <nmguantai@gmail.com>
2020-08-07 18:35:23 +0300

    first commit

---
+++ polls/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.template import loader
+
+# Create your views here.
+#shortcut
+'''def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+'''
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
